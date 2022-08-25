0c98d37ea702c9dcbc5922b3b42123841194dcad
bokken <negibokken@gmail.com>
2022-08-26 07:56:31 +0900

    feat(commits): Add script to fetch and write commits

--- commits/index.ts
+++ commits/index.ts
@@ -1,5 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClient = new PrismaClient();
+
 const axios = require('axios').default;
 const url = 'https://chromium.googlesource.com/chromium/src/+log?format=JSON';
+
+type Commit = {
+    commit: string;
+    title: string;
+    message: string;
+    commitAt: Date;
+}
+
+type CommitResponse = {
+    log: ChromiumCommit[];
+    next: string;
+}
+
+type CommitHash = string;
+
+type AuthorInfo ={
+    name: string;
+    email: string;
+    time: string;
+}
+
+type ChromiumCommit = {
+    commit: CommitHash;
+    tree: CommitHash;
+    parents: CommitHash[];
+    author: AuthorInfo;
+    committer: AuthorInfo;
+    message: string;
+}
 
 (async () => {
     const res = await axios.get(url);
@@ -12,5 +45,39 @@ const url = 'https://chromium.googlesource.com/chromium/src/+log?format=JSON';
     //   ...
     // }
     const firstLineBreak = res.data.indexOf('\n');
-    console.log(JSON.parse(res.data.slice(firstLineBreak)));
+    const data = JSON.parse(res.data.slice(firstLineBreak)) as CommitResponse;
+
+    const commits: Commit[] = data.log.map((log) => {
+
+        const firstLineBreak = log.message.indexOf('\n');
+        const title = log.message.slice(0, firstLineBreak);
+        const message = log.message.slice(firstLineBreak);
+        return {
+            commit: log.commit,
+            title: title,
+            message: message,
+            commitAt: new Date(log.committer.time),
+        }
+    });
+
+    const query = commits.map((commit) => {
+        return prismaClient.commits.upsert({
+            where: {
+                commit: commit.commit,
+            },
+            update: {
+                message: commit.message,
+                title: commit.title,
+            },
+            create: {
+                commit: commit.commit,
+                title: commit.title,
+                message: commit.message,
+                commitAt: commit.commitAt,
+            }
+        })
+    })
+
+    const result = await prismaClient.$transaction([...query]);
+    console.log(result);
 })()
